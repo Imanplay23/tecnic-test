7ec831de8a5eff1a7d861dc46ce9385f839161c0
Imanplay23 <yandisanchez0321@gmail.com>
2025-01-31 18:24:20 -0400

    feat(home): menu and button to go to profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
 ];
 
 @NgModule({
